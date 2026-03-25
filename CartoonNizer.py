3735168f11ed0ebd7ba2dffb4836c52924608b6e
Noodle-Box <tevyn.vergara@yahoo.com>
2026-03-26 00:08:31 +1000

    Added seperate execution files for A1 questions

---
+++ CartoonNizer.py
@@ -0,0 +1,15 @@
+####### Code for Question 3 - ELEC4630 A1 - Tevyn Vergara ######
+
+############################################### Import Libraries ################################################################
+
+from warnings import filters
+from skimage import data, color, feature, filters, io # for inbuilt images
+from skimage.color import *
+from skimage.feature import corner_harris, corner_peaks, corner_subpix
+from skimage.filters import threshold_otsu
+import numpy as np
+from skimage.morphology import dilation, erosion, disk, square, diamond, footprint_rectangle
+import matplotlib.pyplot as plt
+from scipy.ndimage import uniform_filter
+
+################################################ Function Definitions #############################################################
